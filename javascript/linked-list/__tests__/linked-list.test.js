6e71709853f26732d6409c760ae91d0ce36bf859
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-06-17 16:53:18 +0000

    Linked List implementation and testing

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -3,8 +3,60 @@
 // Require our linked list implementation
 const LinkedList = require('../index');
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+// describe('Linked List', () => {
+//   it('works', () => {
+//     expect(true).toBeTruthy();
+//   });
+// });
+
+
+describe('LinkedList', () => {
+  it('Can successfully instantiate an empty linked list', () => {
+    const list = new LinkedList();
+    expect(list.head).toBeNull();
+  });
+  it('Can properly insert into the linked list', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    expect(list.head.value).toBe('a');
+    expect(list.head.next).toBeNull();
+  });
+  it('The head property will properly point to the first node in the linked list', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    expect(list.head.value).toBe('c');
+    expect(list.head.next.value).toBe('b');
+  });
+  it('Can properly insert multiple nodes into the linked list', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    expect(list.head.value).toBe('c');
+    expect(list.head.next.value).toBe('b');
+    expect(list.head.next.next.value).toBe('a');
+  });
+  it('Will return true when finding a value within the linked list that exists', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    expect(list.includes('a')).toBe(true);
+  });
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    expect(list.includes('f')).toBe(false);
+  });
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    const list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    expect(list.toString()).toBe('{ c } -> { b } -> { a } -> NULL');
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,54 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  toString() {
+    let current = this.head;
+    let result = '';
+    while (current) {
+      result += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    result += 'NULL';
+    return result;
+  }
+}
+
+let LinkedLi = new LinkedList();
+LinkedLi.insert('c');
+LinkedLi.insert('b');
+LinkedLi.insert('a');
+console.log(LinkedLi);
+console.log(LinkedLi.includes('f'));
+console.log(LinkedLi.includes('a'));
+console.log(LinkedLi.toString());
 module.exports = LinkedList;
