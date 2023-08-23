49f00ce687f3faa82c743bcef3b54d9ef522153a
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-08-23 20:53:03 +0000

    solved

---
+++ javascript/Merge Sort/index.js
@@ -0,0 +1,44 @@
+function mergeSort(arr) {
+  let n = arr.length;
+
+  if (n > 1) {
+    let mid =  Math.floor(n/2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, n);
+
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+  return arr;
+}
+
+function merge(left, right, arr) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+
+  if (i === left.length) {
+    let remainRight = right.slice(j);
+    arr = arr.splice(k , arr.length-k , ...remainRight);
+  } else {
+    let remainLeft = left.slice(i);
+    arr = arr.splice(k , arr.length-k , ...remainLeft);
+  }
+}
+
+module.exports = {
+  mergeSort,
+};
+
