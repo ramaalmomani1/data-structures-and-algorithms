cb613cba3a6e317f2e95ed6aa6c320fef8bcbe10
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-09-18 11:59:14 +0000

    done

--- javascript/Graph/Graph.js
+++ javascript/Graph/Graph.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const Edge = require("./Edge");
-const Vertext = require("./Vertex");
+// const Vertex = require("./Vertex"); 
 
 class Graph {
   constructor() {
@@ -13,8 +13,10 @@ class Graph {
   }
 
   addEdge(start, end, weight) {
-    if (!this.adjacencyList.has(start) && !this.adjacencyList.has(end)) {
-      console.log("We cannot have an edge without staring and ending Vertex!!");
+    if (!this.adjacencyList.has(start) || !this.adjacencyList.has(end)) {
+      console.log(
+        "We cannot have an edge without starting and ending Vertex!!"
+      );
       return;
     }
 
@@ -22,6 +24,7 @@ class Graph {
     const edge = new Edge(end, weight);
     startVertex.push(edge);
   }
+
   getVertices() {
     return Array.from(this.adjacencyList.keys());
   }
@@ -44,7 +47,7 @@ class Graph {
   size() {
     return this.adjacencyList.size;
   }
-  
+
   breadthFirst(startNode) {
     if (!this.adjacencyList.has(startNode)) {
       return [];
@@ -72,48 +75,83 @@ class Graph {
 
     return result;
   }
+
+  businessTrip(graph, cities) {
+    if (cities.length < 2) {
+      return null;
+    }
+
+    let totalCost = 0;
+    for (let i = 0; i < cities.length - 1; i++) {
+      const startCity = cities[i];
+      const endCity = cities[i + 1];
+
+      const startVertex = graph
+        .getVertices()
+        .find((vertex) => vertex.value === startCity);
+
+      if (!startVertex) {
+        return null;
+      }
+
+      const neighbors = graph.getNeighbors(startVertex);
+
+      const directFlight = neighbors.find(
+        (neighbor) => neighbor.vertex.value === endCity
+      );
+
+      if (!directFlight) {
+        return null;
+      }
+
+      totalCost = totalCost + directFlight.weight;
+    }
+
+    return totalCost;
+  }
 }
 
 module.exports = Graph;
-const graph = new Graph();
 
-const one = new Vertext(1);
-const two = new Vertext(2);
-const three = new Vertext(3);
-const four = new Vertext(4);
-const five = new Vertext(5);
-const six = new Vertext(6);
-graph.addVertex(one);
-graph.addVertex(two);
-graph.addVertex(three);
-graph.addVertex(four);
-graph.addVertex(five);
-graph.addVertex(six);
+// const graph = new Graph();
 
-graph.addEdge(one, two);
-graph.addEdge(one, three, 5);
-graph.addEdge(two, four);
-graph.addEdge(two, five);
-graph.addEdge(four, three);
-graph.addEdge(five, six);
+// const seattle = new Vertex("Seattle");
+// const sanFrancisco = new Vertex("San Francisco");
+// const losAngeles = new Vertex("Los Angeles");
+// const lasVegas = new Vertex("Las Vegas");
+// const newYork = new Vertex("New York");
+// const chicago = new Vertex("Chicago");
 
-// console.log(graph.adjacencyList.entries())
+// graph.addVertex(seattle);
+// graph.addVertex(sanFrancisco);
+// graph.addVertex(losAngeles);
+// graph.addVertex(lasVegas);
+// graph.addVertex(newYork);
+// graph.addVertex(chicago);
 
+// graph.addEdge(seattle, sanFrancisco, 200);
+// graph.addEdge(seattle, losAngeles, 150);
+// graph.addEdge(sanFrancisco, losAngeles, 100);
+// graph.addEdge(sanFrancisco, lasVegas, 300);
+// graph.addEdge(losAngeles, lasVegas, 50);
+// graph.addEdge(newYork, chicago, 200);
 
+// const cost1 = graph.businessTrip(graph, [
+//   "Seattle",
+//   "San Francisco",
+//   "Los Angeles",
+// ]);
+// console.log("trip 1", cost1);
 
-for (const [k, v] of graph.adjacencyList.entries()) {
-  console.log("Key =>", k, "V =>", v);
-  // for(let i = 0; i < v.length; i++) {
-  //   console.log(v[i])
-  // }
-}
+// const cost2 = graph.businessTrip(graph, [
+//   "Seattle",
+//   "San Francisco",
+//   "Las Vegas",
+// ]);
+// console.log("trip 2", cost2);
 
-////////////////
+// const cost3 = graph.businessTrip(graph, ["New York", "Chicago"]);
+// console.log("trip 3", cost3);
 
-console.log("Vertices", graph.getVertices());
-
-const neighbors = graph.getNeighbors(one);
-console.log("Neighbors", neighbors);
-
-console.log("Size ", graph.size());
-console.log('Breadth-First Traversal:', graph.breadthFirst(one));
+// const cost4 = graph.businessTrip(graph, ["San Francisco", "New York"]);
+// console.log("trip 4", cost4);
